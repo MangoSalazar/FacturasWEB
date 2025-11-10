082a3d121b74175cd1e5d9b59558b68f652e9a1b
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-09 23:04:37 -0700

    Cree el servicioFacturas y añadi la base de datos

---
+++ Components/Data/ServicioFacturas.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using FacturasWEB.Components.Data;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FacturasWEB.Components.Data
+{
+    public class ServicioFacturas
+    {
+        private readonly SqliteConnection _connection;
+        private List<Factura> facturas = new List<Factura>();
+
+        public ServicioFacturas(SqliteConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<List<Factura>> obtenerFacturas()
+        {
+            facturas.Clear();
+            var sql = @"
+                SELECT * FROM Facturas;
+                
+                SELECT 
+                    a.ID_articulos,
+                    a.Nombre,
+                    a.Precio,
+                    c.Cantidad,
+                    c.ID_facturas
+                FROM Articulos a
+                JOIN Contiene c ON a.ID_articulos = c.ID_articulos;
+            ";
+
+            using (var multi = await _connection.QueryMultipleAsync(sql))
+            {
+                facturas = (await multi.ReadAsync<Factura>()).ToList();
+                var articulos = (await multi.ReadAsync<Articulo>()).ToList();
+                foreach (var f in facturas)
+                {
+
+                    f.articulos.AddRange(articulos.Where(a => a.ID_facturas == f.ID_facturas));
+                }
+
+                return facturas;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using FacturasWEB.Components;
+using FacturasWEB.Components.Data;
+using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+builder.Services.AddSingleton<ServicioFacturas>();
 
 var app = builder.Build();
 
@@ -23,5 +28,49 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+String ruta = "mibase.db";
+
+using var conexion = new SqliteConnection($"DataSource={ruta}");
+conexion.Open();
+var comando = conexion.CreateCommand();
+comando.CommandText = @"
+
+PRAGMA foreign_keys = ON;
+
+
+CREATE TABLE if not exists
+Articulos (
+    ID_articulos INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nombre TEXT NOT NULL,
+    Precio REAL NOT NULL -- 'REAL' es el tipo para números decimales en SQLite
+);
+
+
+CREATE TABLE if not exists
+Facturas (
+    ID_facturas INTEGER PRIMARY KEY AUTOINCREMENT,
+    Fecha TEXT NOT NULL, 
+    Nombre TEXT NOT NULL 
+);
+
+
+CREATE TABLE if not exists
+Contiene (
+    ID_facturas INTEGER NOT NULL,
+    ID_articulos INTEGER NOT NULL,
+    Cantidad INTEGER NOT NULL,
+    
+
+    PRIMARY KEY (ID_facturas, ID_articulos),
+    
+
+    FOREIGN KEY (ID_facturas) REFERENCES Facturas(ID_facturas)
+        ON DELETE CASCADE,
+    FOREIGN KEY (ID_articulos) REFERENCES Articulos(ID_articulos)
+        ON DELETE RESTRICT
+);
+";
+comando.ExecuteNonQuery();
 
 app.Run();
