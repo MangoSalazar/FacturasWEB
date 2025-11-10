d85a872eded91ea76445e2c001a349547ab3ee5a
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-09 22:55:13 -0700

    Cree la clase articulo

---
+++ Components/Data/Articulo.cs
@@ -0,0 +1,13 @@
+﻿namespace FacturasWEB.Components.Data
+{
+    public class Articulo
+    {
+        public int ID_articulos { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        // Propiedades que se llenarán desde la tabla 'Contiene'
+        public int Cantidad { get; set; }
+        public int ID_facturas { get; set; }
+    }
+}
