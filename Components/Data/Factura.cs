283650248eb4613ead87f55f4e14ac1eadf506d6
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-09 22:57:16 -0700

    Cree la clase Factura

---
+++ Components/Data/Factura.cs
@@ -0,0 +1,10 @@
+﻿namespace FacturasWEB.Components.Data
+{
+    public class Factura
+    {
+        public int ID_facturas { get; set; }
+        public string nombre { set; get; }
+        public string fecha { get; set; }
+        public List<Articulo> articulos { get; set; } = new List<Articulo>();
+    }
+}
