16e76ca8a1591fcee45e2e9f306cb6f21e1aa8c2
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-19 22:59:17 -0700

    clase de estadisticas creada

---
+++ Components/Data/DashboardDatos.cs
@@ -0,0 +1,16 @@
+﻿namespace FacturasWEB.Components.Data
+{
+    public class DashboardDatos
+    {
+        public string ArticuloMasVendido { get; set; } = "Sin datos";
+        public string MesConMasVentas { get; set; } = "Sin datos";
+        public string ClienteMasRepetido { get; set; } = "Sin datos";
+
+        public double PromedioComprasAnuales { get; set; }
+        public double FacturaMasCara { get; set; }
+        public double FacturaMasBarata { get; set; }
+
+        public int AnioMasVentas { get; set; }
+        public int AnioMenosVentas { get; set; }
+    }
+}
